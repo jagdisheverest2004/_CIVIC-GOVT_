129d4b22d1fd1b592305736941bb278ba254fa10
Jagdish Everest <119604053+jagdisheverest2004@users.noreply.github.com>
2025-09-08 19:31:15 +0530

    init commit

---
+++ src/main/java/org/example/civic_govt/CivicGovtApplication.java
@@ -0,0 +1,13 @@
+package org.example.civic_govt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicGovtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivicGovtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/civic_govt/CivicGovtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.civic_govt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicGovtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
