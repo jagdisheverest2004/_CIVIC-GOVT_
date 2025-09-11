6853f25c150e7122a2c77873027b9d1e5479228f
kailash <kailashkiradu@gmail.com>
2025-09-11 18:22:53 +0530

    Update User.java

--- src/main/java/org/example/civic_govt/model/User.java
+++ src/main/java/org/example/civic_govt/model/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idd;
 
     private String username;
 
